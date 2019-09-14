344bdcaca943b08e575eeee6eaeb0d2dbe5842e5
James Akinniranye <jamesniranye@gmail.com>
2019-09-14 01:19:11 +0100

    added base dependecnies

---
+++ src/main/java/io/kiamesdavies/revolut/BigBang.java
@@ -0,0 +1,13 @@
+package io.kiamesdavies.revolut;
+
+/**
+ * Hello world!
+ *
+ */
+public class BigBang
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/io/kiamesdavies/revolut/BigBangTest.java
@@ -0,0 +1,17 @@
+package io.kiamesdavies.revolut;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple BigBang.
+ */
+public class BigBangTest
+
+{
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
